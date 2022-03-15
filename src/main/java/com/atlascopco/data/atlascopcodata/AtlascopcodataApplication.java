88e305de6bedf8ae7f776e7095e847b222128610
Jeroen Remans <jeroen.remans@continuum.be>
2022-03-15 07:48:49 +0100

    Initial commit

---
+++ src/main/java/com/atlascopco/data/atlascopcodata/AtlascopcodataApplication.java
@@ -0,0 +1,13 @@
+package com.atlascopco.data.atlascopcodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlascopcodataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlascopcodataApplication.class, args);
+	}
+
+}
